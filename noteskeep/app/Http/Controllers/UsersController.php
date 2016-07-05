1b71203e6607b265a7adb115d5617900bfe13705
Jelena Drzaic <jelena.drzaic1@gmail.com>
2016-07-05 22:05:54 +0200

    documentation update

--- noteskeep/app/Http/Controllers/UsersController.php
+++ noteskeep/app/Http/Controllers/UsersController.php
@@ -8,8 +8,19 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Auth;
 
+/**
+ * Controller returning information on users
+ * Class UsersController
+ * @package App\Http\Controllers
+ */
 class UsersController extends Controller
 {
+    /**
+     * Method returns the emails that match given query.
+     * Searches through all registered users
+     * @param Request $request
+     * @return mixed
+     */
     public function search(Request $request) {
         $usersService = new UsersService();
         if($request->has('q') == false) {
@@ -27,6 +38,10 @@ class UsersController extends Controller
         return json_encode(array('users' => array_values($usersInfo)));
     }
 
+    /**
+     * Method returns logged user
+     * @return mixed
+     */
     public function index() {
         $user = Auth::user();
         return json_encode(array("user" => $user));

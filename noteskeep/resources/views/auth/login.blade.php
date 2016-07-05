21e9890fde7f87f197f7e0b913ed07d2eed85c45
Jelena Drzaic <jelena.drzaic1@gmail.com>
2016-07-05 22:37:11 +0200

    google login temporary removed

--- noteskeep/resources/views/auth/login.blade.php
+++ noteskeep/resources/views/auth/login.blade.php
@@ -54,7 +54,6 @@
                                     <i class="fa fa-btn fa-sign-in"></i> Login
                                 </button>
                                 <a class="btn btn-primary" href="redirect/facebook">FB Login</a>
-                                <a class="btn btn-primary" href="redirect/google">G+ Login</a>
                                 <a class="btn btn-primary" href="{{ url('/register') }}">Register</a>
                                 <br>
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
